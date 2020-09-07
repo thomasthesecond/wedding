656001b3b9ca977dfd5f4d7a80e4453561759f3c
Tom Cunningham <thomasthesecond@gmail.com>
2020-09-07 15:11:09 -0400

    Add mobile style, clean up and arrow animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-console.log("Page loaded");
+var icon = document.querySelector('.scroll');
+
+window.addEventListener('scroll', (event) => {
+  var scrollYPos = this.scrollY;
+
+  if (scrollYPos === 0) {
+    icon.classList.remove('gone');
+  } else {
+    icon.classList.add('gone');
+  }
+});
